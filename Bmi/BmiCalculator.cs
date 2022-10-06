dd64cae8cfb8ffb93638fdc772692086d2da665f
twdeveloper <twdeveloper@outlook.com>
2022-10-06 11:58:23 +0800

    v1

---
+++ Bmi/BmiCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi
+{
+    public class BmiCalculator
+    {
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public float BMI
+        {
+            get
+            {
+                return Calculate();
+            }
+        }
+
+
+        public float Calculate()
+        {
+            float result = 0;
+            //將float改成int試試看
+            float height = (float)Height / 100;
+            result = Weight / (height * height);
+
+            return result;
+        }
+    }
+}
---
+++ BmiTests/BmiCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bmi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi.Tests
+{
+    [TestClass()]
+    public class BmiCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            //arrange
+            BmiCalculator bmi = new BmiCalculator();
+            bmi.Height = 170;
+            bmi.Weight = 70;
+
+            //act
+            float result = bmi.Calculate();
+
+            //assert
+            Assert.AreEqual("24.22", result.ToString("00.00"));
+        }
+    }
+}
